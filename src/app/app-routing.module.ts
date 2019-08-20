5034338d73fa9536b103446f7a145277925b9ad9
Joshua Seet <joshu5797@gmail.com>
2019-08-20 17:43:38 +0800

    changed comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ReviewsChartComponent } from './reviews-chart/reviews-chart.component';
 import { ReviewsListComponent } from './reviews-list/reviews-list.component';
 
 const routes: Routes = [
-  // path is basically home since the pie chart would be below
+  //default path when URL is empty 
   { path: '', redirectTo: 'reviews-chart', pathMatch: 'full' },
   { path: 'reviews-chart', component: ReviewsChartComponent },
   { path: 'reviews-list', component: ReviewsListComponent }
